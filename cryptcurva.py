5e9fdd732f7db0c5676cc1e93ba0bc1301b47ddd
ivanranet <ivanranet@yandex.ru>
2024-11-24 22:16:38 +0500

    убрал пробелы и лищние строки кода

--- cryptcurva.py
+++ cryptcurva.py
@@ -5,17 +5,15 @@ from tkinter import messagebox as mb
 from tkinter import ttk
 
 
-def update_c_label(event):
+def update_label(event):
     code = combobox.get()
     name = cur[code]
-    c_label.config(text = name)
+    label.config(text = name)
 
 
 def exchange():
     code = combobox.get()
-
     code_dollar = 'usd'
-
     if code and code_dollar:
         try:
             response = requests.get(
@@ -23,10 +21,10 @@ def exchange():
             response.raise_for_status()
             data = response.json()
             if code in data['rates']:
-
                 if code_dollar in data['rates']:
                     exchange_rate = data['rates'][code_dollar]['value'] / data['rates'][code]['value']
-                    mb.showinfo('Курс обмена', f'Курс: 1 {cur[code]} = {exchange_rate:.2f} $. ')
+                    mb.showinfo('Курс обмена',
+                            f'Курс сегодня:\n1 {cur[code]} = {exchange_rate:.2f} $. ')
             else:
                 mb.showerror('Ошибка!', f'Валюта {code} не найдена.')
         except Exception as e:
@@ -40,6 +38,7 @@ window.title('Курсы обмена валют')
 window.geometry('360x180')
 
 Label(text='Выберите криптовалюту:').pack(padx=10, pady=10)
+
 cur = {
     'btc': 'Биткоин',
     'eth': 'Эфириум',
@@ -55,13 +54,10 @@ cur = {
 
 combobox = ttk.Combobox(values = list(cur.keys()))
 combobox.pack(padx=10, pady=10)
-combobox.bind('<<ComboboxSelected>>', update_c_label)
+combobox.bind('<<ComboboxSelected>>', update_label)
 
-c_label = ttk.Label()
-c_label.pack(padx = 10, pady = 10)
-
-# entry = Entry()
-# entry.pack(padx = 10, pady = 10)
+label = ttk.Label()
+label.pack(padx = 10, pady = 10)
 
 Button(text='Курс обмена к доллару США', command=exchange).pack(padx=10, pady=10)
 
